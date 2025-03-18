93ce6d51eeb1e9d26d19b9aaf99333f172f6db34
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-18 17:12:36 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/database');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// Routes API
+app.use('/api/clients', require('./routes/clientRoutes'));
+app.use('/api/voitures', require('./routes/VoitureRoutes'));
+app.use('/api/gps', require('./routes/gpsRoutes'));
+
+// Démarrer le serveur
+const PORT = process.env.API_PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur API en écoute sur le port ${PORT}`));
